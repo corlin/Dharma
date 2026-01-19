54b0b5eb95e983fe0bd87b9f4278d536ad64d83b
corlin <corlin@qq.com>
2026-01-19 19:41:34 +0800

    Initial Commit

---
+++ Dharma/DharmaApp.swift
@@ -0,0 +1,32 @@
+//
+//  DharmaApp.swift
+//  Dharma
+//
+//  Created by 陈永林 on 19/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DharmaApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Dharma/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Dharma
+//
+//  Created by 陈永林 on 19/01/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
